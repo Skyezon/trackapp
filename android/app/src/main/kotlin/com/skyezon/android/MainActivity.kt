56ff4cad710968ac473b9d9c54514bbd9a00ce49
Skyezon <richarddelbert@protonmail.com>
2023-10-19 22:23:27 +0700

    Create splash screen

---
+++ android/app/src/main/kotlin/com/skyezon/android/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.skyezon.android
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/splash.dart
@@ -0,0 +1,46 @@
+
+import 'package:android/pages/search.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SplashState();
+  }
+
+}
+
+class _SplashState extends State<Splash>{
+
+  @override
+  void initState() {
+    super.initState();
+    _navigateSearch();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+                "Splash screen",
+              style: TextStyle(fontSize: 24),
+            )
+          ],
+        ),
+      ),
+
+    );
+  }
+
+  _navigateSearch() async {
+    await Future.delayed(Duration(milliseconds: 1500)).then((value) => {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Search()))
+    });
+  }
+}
